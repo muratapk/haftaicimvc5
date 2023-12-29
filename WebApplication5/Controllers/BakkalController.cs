919605905c419b6690ca483978d4efc9c9957105
muratapk <muratapk@hotmail.com>
2023-12-29 19:11:32 +0300

    Add project files.

---
+++ WebApplication5/Controllers/BakkalController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class BakkalController : Controller
+    {
+        bakkalEntities baglan = new bakkalEntities();
+
+        // GET: Bakkal
+        public ActionResult Index()
+        {
+            var listem = baglan.musteri.ToList();
+            return View(listem);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(musteri gelen)
+        {
+            baglan.musteri.Add(gelen);
+            baglan.SaveChanges();
+            TempData["Success"] = "Yeni Kayıt Yapıldı";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var bul = baglan.musteri.Find(id);
+            return View(bul);
+        }
+        [HttpPost]
+        public ActionResult Edit(musteri gelen)
+        {
+            var bul = baglan.musteri.Find(gelen.musteri_Id);
+            bul.musteri_adi = gelen.musteri_adi;
+            bul.musteri_adres=gelen.musteri_adres;
+            bul.musteri_kul = gelen.musteri_kul;
+            bul.musteri_sifre = gelen.musteri_sifre;
+            baglan.SaveChanges();
+            TempData["Success"] = "Kayıt Güncellendi";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var bul = baglan.musteri.Find(id);
+            return View(bul);
+        }
+        [HttpPost]
+        public ActionResult Delete(musteri gelen)
+        {
+            var bul = baglan.musteri.Find(gelen.musteri_Id);
+            baglan.musteri.Remove(bul);
+            baglan.SaveChanges();
+            TempData["Success"] = "Kayıt Silindi";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication5/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class LoginController : Controller
+    {
+        bakkalEntities baglan = new bakkalEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(musteri gelen)
+        {
+            var bul = baglan.musteri.Where(x => x.musteri_kul==gelen.musteri_kul && x.musteri_sifre==gelen.musteri_sifre).FirstOrDefault();
+            if (bul != null)
+            {
+                TempData["Success"] = "Kayıt Başarılı";
+                return RedirectToAction("Index","Bakkal");
+                
+            }
+            TempData["Error"] = "Kullanıcı Adı veya Şifre Hatalı";
+            return View();
+        }
+    }
+}
